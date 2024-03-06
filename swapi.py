0c14b824be11362cde73dda31575915e0fd7a3aa
mathieu savoure <mathieu.savoure@gmail.com>
2024-03-06 15:57:39 +0100

    ajout code planet sur swapi

--- swapi.py
+++ swapi.py
@@ -65,3 +65,20 @@ def main_vehicles():
     df = pd.DataFrame(vehicles)
     print(df)
 
+def get_star_wars_data(resource):
+    base_url = "https://swapi.dev/api/"
+    url = base_url + resource
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def get_planets():
+    planets_data = get_star_wars_data("planets")
+    return planets_data['results']
+
+def main_planets():
+    planets = get_planets()
+    df = pd.DataFrame(planets)
+    print(df)
