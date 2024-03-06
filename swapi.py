4e824f198f7d84788c8b70044cf768684c531c6c
heloise gaillard <gaillard.heloise@yahoo.com>
2024-03-06 15:42:46 +0100

    ajout swapi et get_vehicles

--- swapi.py
+++ swapi.py
@@ -20,3 +20,20 @@ def main():
     print(df)
 
 
+def get_star_wars_data_vehicles(resource):
+    base_url = "https://swapi.dev/api/"
+    url = base_url + resource
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def get_vehicles():
+    vehicles_data = get_star_wars_data_vehicles("vehicles")
+    return vehicles_data['results']
+
+def main_vehicles():
+    vehicles = get_vehicles()
+    df = pd.DataFrame(vehicles)
+    print(df)
