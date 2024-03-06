31cbfebdf74a3f46758af45b0cdb698718e034ec
heloise gaillard <gaillard.heloise@yahoo.com>
2024-03-06 15:20:26 +0100

    ajout sur le swapi du code vehicule

--- swapi.py
+++ swapi.py
@@ -19,4 +19,20 @@ def main():
     df = pd.DataFrame(starships)
     print(df)
 
+def get_star_wars_data_vehicles(resource):
+    base_url = "https://swapi.dev/api/"
+    url = base_url + resource
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
+def get_vehicles():
+    vehicles_data = get_star_wars_data_vehicles("vehicles")
+    return vehicles_data['results']
+
+def main_vehicles():
+    vehicles = get_vehicles()
+    df = pd.DataFrame(vehicles)
+    print(df)
